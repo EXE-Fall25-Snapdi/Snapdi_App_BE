3b54168579ed3326cd43505b3bd21a0d9bf3c521
Layze04 <layzegot@gmail.com>
2025-09-26 23:14:10 +0700

    add project structure

--- Snapdi/Snapdi.Api/Program.cs
+++ Snapdi/Snapdi.Api/Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Snapdi.Repositories.Models;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+// Add DbContext
+builder.Services.AddDbContext<Snapdi_DB_v1_Context>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
